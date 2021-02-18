8838aaa2f80782a88ddeaf9f77dd9847931a1556
dominik-dopka <dominik.dopka@gmail.com>
2021-02-18 03:21:03 +0100

    Finished project

--- regression.py
+++ regression.py
@@ -1,186 +1,227 @@
-# Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd     
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
-def setupModels():
-    # Importing the dataset
-    # dataset = pd.read_csv('Temperature_Revenue.csv')
-    # dataset = pd.read_csv('Salary_Data.csv')
-    # X = dataset.iloc[:, :-1].values
-    # y = dataset.iloc[:, 1].values
-    
-    # generate regression dataset
+def initialLinearSetup(samples = 1000, features = 1):
     from sklearn.datasets import make_regression
-    X, y = make_regression(n_samples=200, n_features=1, noise=20.0)
-    # print(X)
-    # print(y)
-
-    # Splitting the dataset into the Training set and Test set
+    
+    x, y = make_regression(n_samples=samples, n_features=features, noise=5.0)
+    df = pd.DataFrame(data=x)
+    
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05)
+    train_data = train_test_split(x, y, test_size = 0.25)
     
-    # Linear Models
+    if features != 1:
+        new_array = []
+        i = 0
+        while i < features:
+            new_array = []
+            j = 0
+            
+            while j < len(train_data[2]):
+                new_array.append(train_data[0][j][i])
+                j += 1
+            
+            plt.scatter(new_array, train_data[2], marker='.')
+            i += 1
+            
+    else:
+        plt.scatter(train_data[0], train_data[2], marker='.')
+
+    plt.title('Look of linear problem')
+    plt.show()
+    
+    return df, x, y, train_data
+    
+def initialCurveSetup(samples = 300):    
+    x = 10 * np.random.RandomState(1).rand(samples)
+    x = np.sort(x)
+    y = 2 * x - 5 + np.random.RandomState(1).randn(samples)
+    y = np.sin(x)
+    
+    x = x.reshape(-1, 1)
+    
+    df = pd.DataFrame(data=x)
+    
+    from sklearn.model_selection import train_test_split
+    train_data = train_test_split(x, y, test_size = 0.25)
+    
+    plt.scatter(train_data[0], train_data[2], marker='.')
+    plt.title('Look of sinusoid problem')
+    plt.show()
+    
+    return df, x, y, train_data
+    
+def initialExampleDataSetup():
+    data = pd.read_csv("movie_metadata.csv")
+    
+    # print(data)
+    
+    df = pd.DataFrame(data)
+    x = df.dtypes[df.dtypes!='object'].index
+    x = df[x]
+    x = x.fillna(0)
+    
+    y = x['imdb_score']
+    x.drop(['imdb_score'],axis=1,inplace=True)
+    
+    from sklearn.model_selection import train_test_split
+    train_data = train_test_split(x, y, test_size = 0.05)
+    
+    for column in x:
+        plt.scatter(x[column], y, marker='.')
+    
+    plt.title('Look of example data problem')
+    plt.show()
+    
+    return df, x, y, train_data
+    
+def residualPlot(y_predict, title):
+    plt.rcParams['figure.figsize'] = (6.0, 6.0)
+    preds = pd.DataFrame({"preds":y_predict, "true":y_train})
+    preds["residuals"] = preds["true"] - preds["preds"]
+    preds.plot(x = "preds", y = "residuals",kind = "scatter", marker='.')
+    # plt.plot(y, y, linestyle=':')
+    plt.axhline(y=0, color='black', linestyle='-', linewidth = 1)
+    plt.title(title)
+    plt.show()
+    
+def score(y_predict, name):
+    from sklearn.metrics import mean_squared_error, r2_score, max_error
+    
+    score_mse = mean_squared_error(y_train, y_predict)
+    score_r2 = r2_score(y_train, y_predict)
+    score_me = max_error(y_train, y_predict)
+    
+    score_data.at[name, 'Mean squared error'] = score_mse
+    score_data.at[name, 'Coefficient of determination'] = score_r2
+    score_data.at[name, 'Max error'] = score_me
+    
+def scorePlot():
+    score_data['Mean squared error'].plot(kind='bar')
+    plt.title('Mean squared error')
+    plt.show()
+    
+    score_data['Coefficient of determination'].plot(kind='bar')
+    plt.title('Coefficient of determination')
+    plt.show()
+    
+    score_data['Max error'].plot(kind='bar')
+    plt.title('Max error')
+    plt.show()
+    
+def linearRegression():
     from sklearn.linear_model import LinearRegression
-    linear_model = LinearRegression()
+    model = LinearRegression()
     
+    print(x_train)
+    print(y_train)
+    
+    model.fit(x_train,y_train)
+    y_predict = model.predict(x_train)
+    
+    residualPlot(y_predict, "Residual plot in Linear Regressor")
+    score(y_predict, "Linear")
+    
+def ridgeRegression():   
     from sklearn.linear_model import Ridge
-    ridgeModel = Ridge(alpha=0.99)
+    model = Ridge()
+    model.fit(x_train,y_train)
+    y_predict = model.predict(x_train)
     
+    residualPlot(y_predict, "Residual plot in Ridge Regressor")
+    score(y_predict, "Ridge")
+    
+def lassoRegression():
     from sklearn.linear_model import Lasso
-    lassoModel = Lasso(alpha=0.38)
+    # model = Lasso()
+    model = Lasso(alpha=0.38)
+    model.fit(x_train,y_train)
+    y_predict = model.predict(x_train)
     
+    residualPlot(y_predict, "Residual plot in Lasso Regressor")
+    score(y_predict, "Lasso")
+    
+def elasticNet():
     from sklearn.linear_model import ElasticNet
-    elasticNetModel = ElasticNet(alpha=0.1, l1_ratio=0.86)
+    model = ElasticNet(alpha=0.1, l1_ratio=0.86)
+    # model = ElasticNet()
+    model.fit(x_train,y_train)
+    y_predict = model.predict(x_train)
     
-    # Nonlinear Models
+    residualPlot(y_predict, "Residual plot in Elastic Net Regressor")
+    score(y_predict, "Elastic Net")
+    
+def decisionTreeRegression():   
     from sklearn.tree import DecisionTreeRegressor
-    decisionTree_model = DecisionTreeRegressor()
+    model = DecisionTreeRegressor(max_depth = 5)
+    # model = DecisionTreeRegressor()
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_train)
     
+    residualPlot(y_predict, "Residual plot in Decision Tree Regressor")
+    score(y_predict, "Decision Tree")
+    
+def kNeighborsRegression():
     from sklearn.neighbors import KNeighborsRegressor
-    nearestNeighborsModel = KNeighborsRegressor()
+    model = KNeighborsRegressor()
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_train)
     
+    residualPlot(y_predict, "Residual plot in K-Neighbors Regressor")
+    score(y_predict, "K-Neighbors")
+    
+def svmRegression(isCurve):
     from sklearn.svm import SVR
     from sklearn import preprocessing
     from sklearn.pipeline import make_pipeline
-       
-    svrModel = make_pipeline(
-        preprocessing.StandardScaler(),
-        SVR(kernel='rbf', epsilon=0.01, C=100, gamma = 0.01)
-    )
+    if isCurve == True:
+        model = SVR()
+    else:
+        model = make_pipeline(
+            preprocessing.StandardScaler(),
+            SVR(kernel='rbf', epsilon=0.01, C=100, gamma = 0.01)
+        )
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_train)
     
-    # https://stackoverflow.com/questions/29819428/normalization-or-standardization-data-input-for-svm-scikitlearn
+    residualPlot(y_predict, "Residual plot in SVM Regressor")
+    score(y_predict, "SVM")
     
-    # Linear Train
-    linear_model.fit(X_train, y_train)
-    ridgeModel.fit(X_train, y_train)
-    lassoModel.fit(X_train, y_train)
-    elasticNetModel.fit(X_train, y_train)
-    
-    # Nonlinear Training
-    decisionTree_model.fit(X_train, y_train)
-    nearestNeighborsModel.fit(X_train, y_train)
-    svrModel.fit(X_train, y_train)
-    
-    # Predicting the Test set results
-    y_pred_linear = linear_model.predict(X_test)
-    y_pred_ridge = ridgeModel.predict(X_test)
-    y_pred_lasso = lassoModel.predict(X_test)
-    y_pred_elasticNet = elasticNetModel.predict(X_test)
-    
-    y_pred_decisionTree = decisionTree_model.predict(X_test)
-    y_pred_nearestNeighbors = nearestNeighborsModel.predict(X_test)
-    y_pred_svr = svrModel.predict(X_test)
-    
-    return X_test, y_test, y_pred_linear, y_pred_ridge, y_pred_lasso, y_pred_elasticNet, y_pred_decisionTree, y_pred_nearestNeighbors, y_pred_svr
-    
-def displayLinearPlot(X_test, y_test, y_pred_linear, y_pred_ridge, y_pred_lasso, y_pred_elasticNet):
-    fig = plt.figure('Linear Regression')
 
-    plt.subplot(221)
-    plt.title('Linear Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_linear, color = 'green')
     
-    plt.subplot(222)
-    plt.title('Ridge Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_ridge, color = 'green')
-    
-    plt.subplot(223)
-    plt.title('Lasso Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_lasso, color = 'green')
-    
-    plt.subplot(224)
-    plt.title('Elastic Net Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_elasticNet, color = 'green')
-    
-    plt.show()
-    
-def displayNonlinearPlot(X_test, y_test, y_pred_decisionTree, y_pred_nearestNeighbors, y_pred_svr):
-    fig = plt.figure('Nonlinear Regression')
-
-    plt.subplot(131)
-    plt.title('Decision Tree Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_decisionTree, color = 'green')
-    
-    plt.subplot(132)
-    plt.title('K-Nearest Neighbors Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_nearestNeighbors, color = 'green')
-    
-    plt.subplot(133)
-    plt.title('Support Vector Machines Regression')
-    plt.scatter(X_test, y_test, color = 'red')
-    plt.scatter(X_test, y_pred_svr, color = 'green')
-    
-    plt.show()
-    
-def score(y_test, y_pred):
-
-    from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, max_error
-    
-    score_mse = mean_squared_error(y_test, y_pred)
-    score_r2 = r2_score(y_test, y_pred)
-    score_mae = mean_absolute_error(y_test, y_pred)
-    score_me = max_error(y_test, y_pred)
-    
-    scores = [score_mse, score_r2, score_mae, score_me]
-    
-    print('Mean squared error: %.2f' % score_mse)   
-    print('Coefficient of determination: %.2f' % score_r2) 
-    print('Mean absolute error: %.2f' % score_mae)
-    print('Max error: %.2f' % score_me)
-    
-    return scores
-    
-def displayScorePlot(score_list):
-    score_mse, score_r2, score_mae, score_me = []
-
-    for score in score_list:
-        score_mse.append(score[0])
-        score_r2.append(score[1])
-        score_mae.append(score[2])
-        score_me.append(score[3])
-        
-        # print('\nMean squared error: %.2f' % score[0])   
-        # print('Coefficient of determination: %.2f' % score[1]) 
-        # print('Mean absolute error: %.2f' % score[2])
-        # print('Max error: %.2f' % score[3])
-
 if __name__ == "__main__":
-
-    X_test, y_test, y_pred_linear, y_pred_ridge, y_pred_lasso, y_pred_elasticNet, y_pred_decisionTree, y_pred_nearestNeighbors, y_pred_svr = setupModels()
+    #Linear regression data
+    # df, x, y, train_data = initialLinearSetup()
     
-    print("Linear Model:")
-    score_linear = score(y_test, y_pred_linear)
+    # Multiple linear regression data
+    # df, x, y, train_data = initialLinearSetup(1000, 10)
     
-    print("\nRidge Model:")
-    score_ridge = score(y_test, y_pred_ridge)
+    #Curve data
+    # df, x, y, train_data = initialCurveSetup()
     
-    print("\nLasso Model:")
-    score_lasso = score(y_test, y_pred_lasso)
+    #Example data
+    df, x, y, train_data = initialExampleDataSetup() 
     
-    print("\nElastic Net Model:")
-    score_elasticNet = score(y_test, y_pred_elasticNet)
     
-    print("\nDecision Tree Model:")
-    score_decisionTree = score(y_test, y_pred_decisionTree)
+    x_train, x_test, y_train, y_test = train_data
     
-    print("\nK-Nearest Neighbors Model:")
-    score_nearestNeighbors = score(y_test, y_pred_nearestNeighbors)
+    col = ['Mean squared error', 'Coefficient of determination', 'Max error']
+    models = ['Linear', 'Ridge', 'Lasso', 'Elastic Net', 'Decision Tree', 'K-Neighbors', 'SVM'] 
+    score_data = pd.DataFrame(columns = col, index = models)
     
-    print("\nSupport Vector Machines Model:")
-    score_svm = score(y_test, y_pred_svr)
+    linearRegression()
+    ridgeRegression()
+    lassoRegression()
+    elasticNet()
+    decisionTreeRegression()
+    kNeighborsRegression()
     
-    score_list = [score_linear, score_ridge, score_lasso, score_elasticNet, score_decisionTree, score_nearestNeighbors, score_svm]
-    # print(score_list)
+    # Performs better for linear
+    svmRegression(False)
     
-    # displayScorePlot(score_list)
+    # Performs better for curve
+    # svmRegression(True)
     
-    displayLinearPlot(X_test, y_test, y_pred_linear, y_pred_ridge, y_pred_lasso, y_pred_elasticNet)
-    displayNonlinearPlot(X_test, y_test, y_pred_decisionTree, y_pred_nearestNeighbors, y_pred_svr)
-        
+    scorePlot()
